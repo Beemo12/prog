55fb81bc880eb75eec2665e6b7b45294584d034a
Yassine Benmseseoud <37615@ma-web.nl>
2023-09-29 09:38:16 +0200

    meer opdrachten gedaan

--- les 2/Opdracht B May I come in.js
+++ les 2/Opdracht B May I come in.js
@@ -1,9 +1,17 @@
-var readlineSync = require('readline-sync');
-var age = readlineSync.question('Hallo hoe oud bent u? ');
-if (age <= 17) {
-    console.log('Hallo als u ' + age + ' bent kan ik u niet naar binnen laten');
-}else {
-    console.log('U bent oud genoeg om naar binnen te gaan');
-    
+const readlineSync = require('readline-sync');
+
+function LeeftijdVerificatie() {
+    const leeftijd = parseInt(readlineSync.question('hoe oud ben je? '));
+    if (leeftijd >= 18) {
+        console.log('je bent oud genoeg om naar binnen te gaan.');
+        const hasId = readlineSync.keyInYNStrict('laat me je ID zien');
+        if (hasId) {
+            console.log('je mag naar binnen.');
+        } else {
+            console.log('Sorry, je mag niet naar binnen.');
+        }
+    } else {
+        console.log('Sorry, je bent niet oud genoeg om naar binnen te gaan.');
+    }
 }
-var id = readlineSync.question('Hallo mag ik je ID ien ');
+LeeftijdVerificatie();
